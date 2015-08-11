274f8b891131357b0ae5859b3f621642091c17d1
Moe Szyslak <moe.duffdude@gmail.com>
2015-08-11 16:48:48 +0300

    documenting all the quirks

---
+++ app/directives/directives.list.js
@@ -0,0 +1,8 @@
+/**
+ * summernote
+ * <summernote ng-model="wysiwyg" config="{height: 300,toolbar: [['style', ['style']],['font', ['bold', 'italic', 'underline']],['fontsize', ['fontsize']],['color', ['color']],['para', ['ul', 'ol', 'paragraph']],['height', ['height']],['table', ['table']],['insert', ['link', 'picture', 'hr']],['view', ['fullscreen']]]}" />
+ * search for `.css({display:"",width:Math.min(a.width(),b.width())` inside the summernote.min.js and add `margin: "16px"` property
+ *
+ * calendar
+ * <calendar ng-model="model" />
+ */
