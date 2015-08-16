d369aae0ebaa7fbf731ffb12be6ea8d603b831e9
Moe Szyslak <moe.duffdude@gmail.com>
2015-08-16 11:40:45 +0300

    AFTER destroy

--- app/directives/crop.directive.js
+++ app/directives/crop.directive.js
@@ -15,32 +15,39 @@
           y: '@'
         },
         template:
-          '<div>'+
+          '<div class="col-lg-12">'+
             '<div class="row">'+
               '<div class="col-lg-12 text-center">'+
-                  '<img class="image-responsive" ng-src="{{ src }}" />'+
+                  '<img class="img-responsive" ng-src="{{ src }}" />'+
               '</div>'+
             '</div>'+
-            '<div class="row" ng-if="uncropped">'+
+            '<div class="row" ng-if="uncropped && src.length > 0">'+
               '<div class="col-lg-12 text-center" style="margin-top: 8px;">'+
                 '<button ng-click="crop()" class="btn btn-primary"><i class="fa fa-crop"></i>&nbsp;&nbsp;crop image</button>'+
               '</div>'+
             '</div>'+
           '</div>',
         link: function(scope, element, attribute) {
+          /**
+           * since we're mutating the data we're watching, after we self-mutate
+           * we'll not be listing for any changes for the next 500ms
+           */
+          var _restSelfMutatedFlag = function() {
+            var _selfie = setTimeout(function() {
+              selfMutated = false;
+              clearTimeout(_selfie);
+            }, 500);
+          };
           var selfMutated = false;
           scope.uncropped = true;
           scope.x = isNaN(Number(scope.x)) || scope.x === '' ? 16 : Number(scope.x);
           scope.y = isNaN(Number(scope.y)) || scope.y === '' ? 9 : Number(scope.y);
 
           var unregisterListener = scope.$watch('src', function(newVal, oldVal) {
-            if(selfMutated === true) {
-              selfMutated = false;
-            }
-
-            else {
+            if(selfMutated === false) {
               scope.uncropped = true;
               $($('img', element)[0]).cropper('destroy');
+              $($('img', element)[0]).attr({src: scope.src});
               $($('img', element)[0]).cropper({
                 aspectRatio: scope.x / scope.y,
                 guides: false,
@@ -50,7 +57,10 @@
                 built: function() {
                   scope.$apply(function() {
                     selfMutated = true;
+                    // assigning here doesn't affect the crop view's source
+                    // since it's *outside* the digest cycle
                     scope.src = $($('img', element)[0]).cropper('getCroppedCanvas').toDataURL();
+                    _restSelfMutatedFlag();
                   });
                 }
               });
@@ -58,9 +68,17 @@
           });
 
           scope.crop = function() {
-            selfMutated = true;
-            scope.src = $($('img', element)[0]).cropper('getCroppedCanvas').toDataURL();
-            $($('img', element)[0]).cropper('destroy');
+            try {
+              var newSrc = $($('img', element)[0]).cropper('getCroppedCanvas').toDataURL();
+              selfMutated = true;
+              scope.src = newSrc;
+              $($('img', element)[0]).cropper('destroy');
+              $($('img', element)[0]).attr({src: scope.src});
+              _restSelfMutatedFlag();
+            } catch(e) {
+              console.warn('an invalid src was given, please make sure the image src is properly encoded i.e. to `DataURL`');
+            }
+
             scope.uncropped = false;
           };
 
