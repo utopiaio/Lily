b010a215db97042d87be26f9bcd1129bd8d32e88
Moe Szyslak <moe.duffdude@gmail.com>
2015-08-16 09:52:08 +0300

    one `text-center` to rule em' all
    - `col-lg-3` > `col-lg-4`
    - `col-lg-4` > `col-lg-6`

--- app/directives/documentlist.directive.js
+++ app/directives/documentlist.directive.js
@@ -15,22 +15,22 @@
         template:
         '<div class="row">'+
           '<span ng-if="multiple === true">'+
-            '<div class="col-lg-3 col-md-4 col-sm-12 col-xs-12" ng-repeat="document in src track by $index">'+
-              '<div class="thumbnail text-center" style="border: none;">'+
+            '<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 text-center" ng-repeat="document in src track by $index">'+
+              '<div class="thumbnail" style="border: none;">'+
                 '<i class="fa fa-file-text-o" style="font-size: 72px;"></i>'+
                 '<p ng-bind="document.name" style="margin-top: 8px;"></p>'+
-                '<div class="caption text-center">'+
+                '<div class="caption">'+
                   '<button type="button" ng-dblclick="removeDocAt($index)" class="btn btn-danger"><i class="fa fa-trash-o"></i>&nbsp;&nbsp;double click to delete</button>'+
                 '</div>'+
               '</div>'+
             '</div>'+
           '</span>'+
           '<span ng-if="multiple === false && src.hasOwnProperty(\'name\')">'+
-            '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+
-              '<div class="thumbnail text-center" style="border: none;">'+
+            '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">'+
+              '<div class="thumbnail" style="border: none;">'+
                 '<i class="fa fa-file-text-o" style="font-size: 72px;"></i>'+
                 '<p ng-bind="src.name" style="margin-top: 8px;"></p>'+
-                '<div class="caption text-center">'+
+                '<div class="caption">'+
                   '<button type="button" ng-dblclick="clearDoc()" class="btn btn-danger"><i class="fa fa-trash-o"></i>&nbsp;&nbsp;double click to delete</button>'+
                 '</div>'+
               '</div>'+
