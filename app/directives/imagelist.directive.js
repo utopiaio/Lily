f4c2f7910e10bc96be125a99a095180a691f8d7c
Moe Szyslak <moe.duffdude@gmail.com>
2015-08-16 14:10:16 +0300

    ONLY multiple
    - `image-list` now ONLY concerned with multiple upload
    - for single image display use `crop`

--- app/directives/imagelist.directive.js
+++ app/directives/imagelist.directive.js
@@ -15,39 +15,19 @@
           y: '@'
         },
         template:
-        '<div class="col-lg-12">'+
-          '<div class="row" ng-if="multiple === true">'+
-            '<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12" ng-repeat="image in src track by $index">'+
-              '<div class="thumbnail text-center" style="border: none;">'+
-                '<crop src="src[$index]" x="{{ x }}" y="{{ y }}" />'+
-                '<div class="caption">'+
-                  '<button type="button" ng-dblclick="removeImageAt($index)" class="btn btn-danger"><i class="fa fa-trash-o"></i>&nbsp;&nbsp;double click to delete</button>'+
-                '</div>'+
-              '</div>'+
+          '<div class="row" ng-show="isArray">'+
+            '<div class="col-lg-4 col-md-6" ng-repeat="image in src track by $index">'+
+              '<crop src="src[$index]" />'+
+              '<button type="button" ng-dblclick="removeImageAt($index)" class="btn btn-danger" style="margin-top: 8px;"><i class="fa fa-trash-o"></i>&nbsp;&nbsp;double click to delete</button>'+
             '</div>'+
-          '</div>'+
-          '<div class="row" ng-if="multiple === false && src.length > 0">'+
-            '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+
-              '<div class="thumbnail text-center" style="border: none;">'+
-                '<crop src="src" x="{{ x }}" y="{{ y }}" />'+
-                '<div class="caption">'+
-                  '<button type="button" ng-dblclick="clearImage()" class="btn btn-danger"><i class="fa fa-trash-o"></i>&nbsp;&nbsp;double click to delete</button>'+
-                '</div>'+
-              '</div>'+
-            '</div>'+
-          '</div>'+
-        '</div>',
+          '</div>',
         controller: function($scope, $element, $attrs) {
           var unregisterListener = $scope.$watch('src', function(newVal, oldVal) {
-            $scope.multiple = angular.isString(newVal) ? false : true;
+            $scope.isArray = angular.isArray(newVal) ? true : false;
           });
 
           $scope.removeImageAt = function(index) {
             $scope.src.splice(index, 1);
-          };
-
-          $scope.clearImage = function() {
-            $scope.src = '';
           };
 
           $scope.$on('$destroy', function() {
