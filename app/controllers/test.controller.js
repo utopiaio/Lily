88720bfe3299ae3ec92f9614b89021060c063544
Moe Szyslak <moe.duffdude@gmail.com>
2015-09-10 02:59:53 +0300

    merging...

--- app/controllers/test.controller.js
+++ app/controllers/test.controller.js
@@ -10,8 +10,29 @@
   function TestController() {
     var vm = this;
 
-    vm.date = '';
-    vm.images = '';
-    vm.files = '';
+    // calendar
+    // vm.d1 = '1991-09-08T00:00:00+00:00';
+    // vm.d2 = '1991-09-08';
+    // vm.d3 = '00:00:00+00';
+    // vm.d1 = 'Invalid Date';
+    // vm.d2 = 'Invalid Date';
+    // vm.d3 = 'Invalid Date';
+    vm.d1 = '';
+    vm.d2 = '';
+    vm.d3 = '';
+    // vm.d1 = null;
+    // vm.d2 = null;
+    // vm.d3 = null;
+
+    // document
+    vm.doc1 = null;
+    vm.doc2 = null;
+
+    // image
+    vm.image1 = [];
+    vm.image2 = null;
+
+    // wysiwyg
+    vm.wysiwyg = '<b>wysiwyg</b>';
   }
 })(window.angular);
