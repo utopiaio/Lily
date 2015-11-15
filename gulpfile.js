d358f6c534dd906754876ea781a51b1a69a4b69e
Moe Szyslak <moe.duffdude@gmail.com>
2015-11-15 18:07:14 +0300

    gulpfile.js prepared for vue

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,46 @@
-var path = require('path'),
-    gulp = require('gulp'),
-    less = require('gulp-less'),
-    concat = require('gulp-concat'),
-    livereload = require('gulp-livereload');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
+var vueify = require('vueify');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var path = require('path');
 
-gulp.task('reload', function() {
-  return gulp
-    .src(['./index.html',
-          './app/**/*.html',
-          './app/**/*.css',
-          './app/**/*.js'])
-    .pipe(livereload());
-});
+var watchList = {
+  browserify: ['./app/**/*.vue'],
+  less: ['./app/**/*.less']
+};
 
-gulp.task('compile-less', function() {
+gulp.task('less', function() {
   return gulp
-    .src(['./app/**/*.less'])
+    .src(watchList.less)
     .pipe(less({
       paths: [path.join(__dirname, 'less', 'includes')]
     }))
     .pipe(concat('style.css'))
-    .pipe(gulp.dest('./app/dist/style'));
+    .pipe(gulp.dest('./app/dist'))
+    .pipe(livereload());
+});
+
+gulp.task('browserify', function() {
+  return gulp.src(['app.js'])
+    .pipe(rename({suffix: '.browserifyed'}))
+    .pipe(browserify({transform: ['vueify']}))
+    .pipe(gulp.dest(''))
+    .pipe(livereload());
+});
+
+gulp.task('watch-browserify', function() {
+  gulp.watch(watchList.browserify, ['browserify']);
 });
 
 gulp.task('watch-less', function() {
-  return gulp
-    .watch(['./app/**/*.less'], ['compile-less']);
-});
-
-gulp.task('watch-app', function() {
-  return gulp
-    .watch(['./index.html',
-            './app/**/*.html',
-            './app/**/*.js',
-            './app/**/*.css'], ['reload']);
+  gulp.watch(watchList.less, ['less']);
 });
 
 gulp.task('default', function() {
   livereload.listen();
-  gulp.start('watch-app');
+  livereload();
+  gulp.start('watch-browserify');
   gulp.start('watch-less');
 });
