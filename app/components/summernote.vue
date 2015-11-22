be9abb61e4b53763b727fc4d910c78d5aeb9ad19
Moe Szyslak <moe.duffdude@gmail.com>
2015-11-22 12:28:26 +0300

    summernote
    - it can't be that easy
    - only have basic options to setup api...soon we'll dispatch all events accordingly

---
+++ app/components/summernote.vue
@@ -0,0 +1,38 @@
+<script>
+  /**
+   * @module summoernote
+   *
+   * summernote component - basic initiation, destroy and update on change
+   */
+  export default {
+    install(Vue, options) {
+      Vue.component('summernote', {
+        props: {
+          model: {
+            type: String,
+            required: true,
+            twoWay: true,
+            default: ''
+          },
+          options: {
+            type: Object,
+            required: false,
+            twoWay: false,
+            default() { return {height: 300,toolbar: [['style', ['style']],['font', ['bold', 'italic', 'underline']],['fontsize', ['fontsize']],['color', ['color']],['para', ['ul', 'ol', 'paragraph']],['height', ['height']],['table', ['table']],['insert', ['link', 'picture', 'hr']],['view', ['fullscreen']]]}; }
+          }
+        },
+        template: `<div></div>`,
+        ready() {
+          this.__summernote = $(this.$el).summernote(this.options);
+          this.__summernote.code(this.model);
+          this.__summernote.on('summernote.change', (customEvent, contents, $editable) => {
+            this.model = contents;
+          });
+        },
+        beforeDestroy() {
+          this.__summernote.destroy();
+        }
+      });
+    }
+  };
+</script>
