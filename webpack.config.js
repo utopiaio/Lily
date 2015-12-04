ceb8ba322de1c9b6a4c2fab6120fcc42c381d530
Moe Szyslak <moe.duffdude@gmail.com>
2015-12-04 15:51:40 +0300

    new config!

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,60 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   entry: './app.js',
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'app/dist'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
       // css
-      { test: /\.css$/, loader: 'style!css' },
-
-      // font (bootstrap, font-awesome)
-      { test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=./app/dist/[name].[ext]' },
-
-      // image
-      { test: /\.(jp(e)?g)|png/, loader: 'file-loader?name=./app/dist/[name].[ext]'},
+      { test: /\.css$/, loader: 'style-loader!css' },
 
       // less
-      { test: /\.less$/, loader: 'style!css!less' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+
+      // image
+      { test: /\.(jpg|png|gif)$/, loader: 'file-loader?name=dist/[name].[ext]'},
+
+      // this is where all non web-pack / global stuff are loaded
+      { test: /(trix)\.js$/, loader: 'script-loader'},
+
+      // fonts
+      // { test: /bootstrap\/js\//, loader: 'imports-loader?jQuery=jquery' },
+      { test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=dist/[name].[ext]' },
 
       // vue
-      { test: /\.vue$/, loader: 'vue' },
+      { test: /\.vue$/, loader: 'vue-loader' },
 
       // babel
       { test: /\.babel$/, loader: 'babel', query: { presets: ['es2015'] } }
     ]
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ]
 };
+
+if(process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'production'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ];
+} else {
+  module.exports.devtool = '#source-map';
+}
