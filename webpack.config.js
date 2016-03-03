2f4ad2c91876675ce8dd31dc5e54e7b483101523
Moe Szyslak <moe.duffdude@gmail.com>
2016-03-03 17:31:40 +0300

    webpack config update
    - path join
    - same argument as webpack is now passed

--- webpack.config.js
+++ webpack.config.js
@@ -31,19 +31,17 @@ module.exports = {
       { test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=static/[name].[ext]' },
 
       // babel
-      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+      { test: /\.js$/, loader: 'babel', include: path.join(__dirname, 'app')},
 
       // vue
       { test: /\.vue$/, loader: 'vue-loader' }
-
     ]
   }
 };
 
-// I'm missing something, NODE_ENV=procution throws error. So I'm working around
-// it; for now, to issue a production webpack bundle
-// $ node_modules/.bin/webpack --production
-if(process.argv.indexOf('--production') > -1) {
+// depending on the arugments passes we'll be adding specific
+// plugins to webpack else we'll have production build even on webpack dev server
+if (process.argv.indexOf('-p') > -1) {
   module.exports.devtool = '#source-map';
   module.exports.plugins = [
     new webpack.ProvidePlugin({
